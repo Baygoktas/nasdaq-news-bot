213f7e0815458306e12ac5b7f4bccfeeab0801d9
Baygoktas <98412584+Baygoktas@users.noreply.github.com>
2026-08-22 13:00:43 +0300

    index.js

---
+++ src/index.js
@@ -0,0 +1,9 @@
+export default {
+  async fetch(request, env) {
+    return new Response("Nasdaq Haber Bot aktif.");
+  },
+
+  async scheduled(controller, env, ctx) {
+    console.log("Cron çalıştı.");
+  }
+};
