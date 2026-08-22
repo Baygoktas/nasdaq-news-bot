bf0b8f6faba04e15428d800a4d6270f05d47faa2
Baygoktas <98412584+Baygoktas@users.noreply.github.com>
2026-08-22 13:05:50 +0300

    Delete src directory

--- src/index.js
+++
@@ -1,9 +0,0 @@
-export default {
-  async fetch(request, env) {
-    return new Response("Nasdaq Haber Bot aktif.");
-  },
-
-  async scheduled(controller, env, ctx) {
-    console.log("Cron çalıştı.");
-  }
-};
